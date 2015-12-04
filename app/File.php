6d40b8d20c3b20bcebe0ad283dd88253f94aa912
Curtis Wilcox <extra808@m.cognize.org>
2015-12-04 07:40:05 -0500

    add salt to hash

--- app/File.php
+++ app/File.php
@@ -12,9 +12,13 @@ class File extends Model
         return $this->belongsToMany('\App\Course')->withTimestamps();;
     }
 
+    // mutator to create path based on file name
     public function setNameAttribute($value) {
         $this->attributes['name'] = $value;
-        $this->attributes['path'] = md5($value);
+        
+        // salt added to prevent path guessing for known file names
+        $salt = 'HAT';
+        $this->attributes['path'] = md5($value . $salt);
     }
 
 }
