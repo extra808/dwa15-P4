b012621774a72a2b88c1a953a7148a4f801cc1ee
Curtis Wilcox <extra808@m.cognize.org>
2015-12-06 12:58:41 -0500

    move text fields to Blade partial

--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -10,20 +10,7 @@
     <form action="/students" method="POST">
         <input type='hidden' value='{{ csrf_token() }}' name='_token'>
 
-        <div class="row">
-            <div class="input-group col-md-2">
-                <label class="input-group-addon" for="initials">Initials</label>
-                <input class="form-control" type="text" name="initials" id="initials"
-                value="{{ old('initials', '') }}">
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="input-group col-md-3">
-                <label class="input-group-addon" for="external_id">ID</label>
-                <input class="form-control" type="text" name="external_id" id="external_id" value="{{ old('external_id', '') }}">
-            </div>
-        </div>
+@include('student.input')
 
         <input class="btn btn-submit" type="submit" name="add" value="Add Student">
     </form>
---
+++ resources/views/student/input.blade.php
@@ -0,0 +1,14 @@
+        <div class="row">
+            <div class="input-group col-md-2">
+                <label class="input-group-addon" for="initials">Initials</label>
+                <input class="form-control" type="text" name="initials" id="initials"
+                value="{{ old('initials', '') }}">
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="input-group col-md-3">
+                <label class="input-group-addon" for="external_id">ID</label>
+                <input class="form-control" type="text" name="external_id" id="external_id" value="{{ old('external_id', '') }}">
+            </div>
+        </div>
