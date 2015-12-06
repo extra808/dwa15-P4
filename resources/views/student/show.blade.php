da32411a088576bdbdde61211416b325a8b6559f
Curtis Wilcox <extra808@m.cognize.org>
2015-12-06 12:54:48 -0500

    remove button toggle, move submit back i btn-group

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -9,16 +9,19 @@
 
     <p>Last Modified: {{ $student->updated_at }}<br>
     </p>
-    <div class="btn-group" role="group" data-toggle="buttons">
-        <form action="" method="POST">
-            <input type='hidden' value='{{ csrf_token() }}' name='_token'>
 
+    <form action="{{ $_SERVER['REQUEST_URI'] }}" method="POST">
+        <input type="hidden" name="_method" value="DELETE">
+        <input type='hidden' value='{{ csrf_token() }}' name='_token'>
+
+        <div class="btn-group" role="group">
             <a class="btn btn-default" href="/{{ $_SERVER['REQUEST_URI'] }}/create">Add Course</a>
             <a class="btn btn-default" href="/{{ $_SERVER['REQUEST_URI'] }}/edit">Edit</a>
 
             <input class="btn btn-default" type="submit" name="delete" value="Delete">
-        </form>
-    </div>
+        </div>
+
+    </form>
 
     <ul>
     @foreach($courses as $course)
