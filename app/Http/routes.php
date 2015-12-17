2c445123a1e28d5ae6903cf115f1c82350f3b43e
Curtis Wilcox <extra808@m.cognize.org>
2015-12-16 20:46:01 -0500

    use try catch in social login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,23 +16,32 @@ Route::get('/authorize', function() {
 });
 
 Route::get('google/login', function() {
-    $userDeets;
-    SocialAuth::login('google', function ($user, $userDetails) {
-        // "host domain" account must match
-        if($userDetails->raw['hd'] == 'cognize.org') {
-            $user->nickname = $userDetails->nickname;
-            $user->name = $userDetails->full_name;
-            $user->profile_image = $userDetails->avatar;
-            $user->save();
+    try {
+        SocialAuth::login('google', function ($user, $userDetails) {
+            // "host domain" account must match
+            if($userDetails->raw['hd'] == 'cognize.org') {
+                $user->name = $userDetails->full_name;
+                $user->save();
+    }
+    catch (ApplicationRejectedException $e) {
+        // User rejected application
+        Session::flash('flash_message', 'User rejected application');
+        abort(403, 'Forbbiden');
+    }
+    catch (InvalidAuthorizationCodeException $e) {
+        // Authorization was attempted with invalid
+        // code,likely forgery attempt
+        Session::flash('flash_message', 'Authorization was attempted with invalid code');
+        abort(403, 'Forbbiden');
+    }
 
-            dd($userDetails);
-        }
-        else {
-            Session::flash('flash_message', $userDetails->email .' not allowed to access this application');
-            abort(403, 'Forbbiden');
-        }
-    });
-    return 'Google authorized';
+    // Current user is now available via Auth facade
+    $user = Auth::user();
+
+    dd($user);
+    dd($userDetails);
+
+    return Redirect::intended();
 });
 
 Route::get('/', function () {
