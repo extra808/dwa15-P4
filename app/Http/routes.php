d40cbac8257a3bf4af6f6fb37ee57d45211be0b0
Curtis Wilcox <extra808@m.cognize.org>
2015-12-17 20:38:53 -0500

    remove GitHub OAuth

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,62 +14,6 @@ use SocialNorm\Exceptions\InvalidAuthorizationCodeException;
 |
 */
 
-Route::get('/github/authorize', function() {
-    return SocialAuth::authorize('github');
-});
-
-Route::get('github/login', function() {
-    try {
-        SocialAuth::login('github', function ($user, $userDetails) {
-
-            $nameCheck = '';
-            $userCheck;
-            // get authenticated user's name
-            if($userDetails->full_name) {
-                $nameCheck = $userDetails->full_name;
-            }
-            else {
-                $nameCheck = $userDetails->nickname;
-            }
-
-            // is name in database?
-            if ($userCheck = \ATC\User::where('name', '=', $nameCheck) ->first() ) {
-                if ($userCheck->name == 'extra808') {
-                    $user->email = $userDetails->email;
-                    $user->save();
-                }
-                else {
-                    Session::flash('flash_message', 'You are not staff');
-                    abort(403, 'Forbbiden');
-                }
-            }
-            else {
-                Session::flash('flash_message', 'No such user');
-                abort(403, 'Forbbiden');
-            }
-        });
-    }
-    catch (ApplicationRejectedException $e) {
-        // User rejected application
-        Session::flash('flash_message', 'User rejected application');
-        abort(403, 'Forbbiden');
-    }
-    catch (InvalidAuthorizationCodeException $e) {
-        // Authorization was attempted with invalid
-        // code,likely forgery attempt
-        Session::flash('flash_message', 'Authorization was attempted with invalid code');
-        abort(403, 'Forbbiden');
-    }
-
-    // Current user is now available via Auth facade
-    $user = Auth::user();
-
-    dd($user);
-    dd($userDetails);
-
-    return Redirect::intended();
-});
-
 Route::get('/google/authorize', function() {
     return SocialAuth::authorize('google');
 });
