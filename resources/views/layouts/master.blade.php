28c5eeafe81bdfca3ccc4d3b80015948e7661fd5
Curtis Wilcox <extra808@m.cognize.org>
2015-12-20 09:27:51 -0500

    remove extraneous error message conditional

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -90,13 +90,6 @@ $sitetitle = 'ATC Delivery';
             </div>
         @endif
 
-@if(count($errors) > 0)
-    <ul class='alert alert-warning' role="alert">
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
         {{-- Main page content will be yielded here --}}
         @yield('content')
       </div>
